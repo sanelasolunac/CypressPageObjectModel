f29aa4e58e0584cd295b7364bdd7d1b6b2718155
ssolunac <sanela.solunac@endava.com>
2023-02-24 14:18:27 +0100

    Modified NavBar Component

--- cypress/page-objects/components/NavBar.js
+++ cypress/page-objects/components/NavBar.js
@@ -10,4 +10,17 @@ export default class NavBar {
 	static clickOnSignInButton() {
 		cy.get('#signin_button').click()
 	}
+
+	static clickOnSettingsButton() {
+		cy.get(':nth-child(2) > .dropdown-toggle').click()
+	}
+
+	static clickOnLogoutButton() {
+		cy.contains('username').click()
+		cy.get('#logout_link').click()
+	}
+
+	static displaySignInButton() {
+		cy.isVisible('#signin_button')
+	}
 }
